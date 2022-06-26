b360c709be39c297c7f689d0b7381a246d785954
Thijs Wijnmaalen <thijs@wijnmaalen.name>
2022-06-26 16:44:55 +0200

    Added lerna with 2 packages: stencil-library and vue-library

---
+++ packages/stencil-library/stencil.config.ts
@@ -0,0 +1,26 @@
+import { Config } from '@stencil/core';
+import { vueOutputTarget } from '@stencil/vue-output-target';
+
+export const config: Config = {
+  namespace: 'stencil-library',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    vueOutputTarget({
+      componentCorePackage: 'stencil-library',
+      proxiesFile: '../vue-library/src/components.ts',
+    }),
+  ],
+};
---
+++ packages/vue-library/__tests__/vue-library.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const vueLibrary = require('..');
+
+describe('vue-library', () => {
+    it('needs tests');
+});
